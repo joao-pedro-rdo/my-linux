ac0e90b95bea4dd927ce0ee1186a2de1b3e20288
João Pedro Ramos <122635721+joao-pedro-rdo@users.noreply.github.com>
2024-07-19 10:33:45 -0300

    Update install-dracula.sh

--- install-dracula.sh
+++ install-dracula.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-# Instalando o tema Dracula para o terminal GNOME
-#git clone https://github.com/dracula/gnome-terminal
-#cd gnome-terminal
-#./install.sh
-#cd ..
-#rm -rf gnome-terminal
+#  TEM QUE ESTAR NO DIRETORIO RAIZ 
+cd ..
+#Instalando o tema Dracula para o terminal GNOME
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+./install.sh
+cd ..
+rm -rf gnome-terminal
